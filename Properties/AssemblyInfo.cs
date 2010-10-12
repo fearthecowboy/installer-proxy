f82bdcaeb54eea3ef1b2e740b0423902fcd7bf07
garretts <garretts@microsoft.com>
2010-10-12 10:06:24 -0700

    initial import

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="CoApp Project">
+//     Copyright (c) 2010 Garrett Serack. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("coapp-redirector-editor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("coapp-redirector")]
+[assembly: AssemblyCopyright("Copyright © 2010 Garrett Serack, CoApp Contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("01bcd791-f775-455d-96d1-266a0d663696")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StubWindow.cs
@@ -0,0 +1,143 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="CoApp Project">
+//     Copyright (c) 2010 Garrett Serack. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace coapp_stub {
+    using System;
+    using System.Windows.Forms;
+    using System.IO;
+    using Microsoft.Win32;
+
+    public partial class StubWindow :Form {
+
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new StubWindow());
+        }
+
+        public StubWindow() {
+            InitializeComponent();
+            textBox1.Text = CoAppInstallerPath;
+            button2.Enabled = false;
+        }
+
+        private const string CoAppRegRoot = @"Software\CoApp";
+        private const string CoAppRegRoot2 = @"Software\Wow6432Node\CoApp";
+
+        private const string CoAppInstallerKey= @"CoAppInstaller";
+
+        private static string CoAppInstallerPath {
+            get {
+                RegistryKey regkey = null;
+                string result = null;
+
+                try {
+                    regkey = Registry.LocalMachine.CreateSubKey(CoAppRegRoot);
+
+                    if(null != regkey)
+                        result = regkey.GetValue(CoAppInstallerKey, null) as string;
+                }
+                catch {
+                }
+                finally {
+                    if(null != regkey)
+                        regkey.Close();
+                }
+
+                if(string.IsNullOrEmpty(result) && IntPtr.Size == 8) {
+                    try { // x64 platform, check if the x86 key is set.
+                        regkey = Registry.LocalMachine.CreateSubKey(CoAppRegRoot2);
+
+                        if(null != regkey) {
+                            result = regkey.GetValue(CoAppInstallerKey, null) as string;
+                            CoAppInstallerPath = result; // make sure both copies are to this value.
+                        }
+                    }
+                    catch {
+                    }
+                    finally {
+                        if(null != regkey)
+                            regkey.Close();
+                    }
+                    
+                }
+
+                return result;
+            }
+
+            set {
+                RegistryKey regkey = null;
+                try {
+                    regkey = Registry.LocalMachine.CreateSubKey(CoAppRegRoot);
+
+                    if(null == regkey)
+                        return;
+
+                    regkey.SetValue(CoAppInstallerKey, value);
+                }
+                catch {
+                }
+                finally {
+                    if(null != regkey)
+                        regkey.Close();
+                }
+
+                
+                if(IntPtr.Size == 8) { // x64 platform, set the x86 key as well.
+                    try {
+                        regkey = Registry.LocalMachine.CreateSubKey(CoAppRegRoot2);
+
+                        if(null == regkey)
+                            return;
+
+                        regkey.SetValue(CoAppInstallerKey, value);
+                    }
+                    catch {
+                    }
+                    finally {
+                        if(null != regkey)
+                            regkey.Close();
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.AutoUpgradeEnabled = true;
+            openFileDialog1.Filter = "Program files (*.exe)|*.exe";
+            openFileDialog1.Title = "Select Target EXE";
+            openFileDialog1.DefaultExt = ".exe";
+            openFileDialog1.ShowDialog();
+
+            if(File.Exists(openFileDialog1.FileName))
+                textBox1.Text = openFileDialog1.FileName;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            textBox1.Text = textBox1.Text.Trim();
+
+            if(!string.IsNullOrEmpty(textBox1.Text) && !File.Exists(textBox1.Text)) {
+                if(MessageBox.Show("Target program [" + textBox1.Text + "] does not exist. Save Anyway?","Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+                    return;
+            }
+            CoAppInstallerPath = textBox1.Text;
+            button2.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            button2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            textBox1.Text = CoAppInstallerPath;
+            button2.Enabled = false;
+        }
+    }
+}
+
