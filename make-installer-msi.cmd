486a9169a9f4b9bc137e456ba8f1374e8c17e26a
Garrett Serack <garretts@microsoft.com>
2011-04-26 14:15:36 -0700

    updates wrt move to github

--- make-installer-msi.cmd
+++ make-installer-msi.cmd
@@ -2,7 +2,7 @@
 cd %~dp0
 
 echo "=== Sign EXE ==="
-coapp-simplesigner.exe "coapp.installer.exe"
+..\..\..\..\solution\tools\simplesigner\simplesigner.exe "coapp.installer.exe"
 
 echo "=== Create Manifest + Catalog ==="
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\mt.exe" -manifest "coapp.installer.manifest" -hashupdate -makecdfs
@@ -13,14 +13,14 @@ echo "=== Create Policy Manifest + Catalog ==="
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\MakeCat.Exe" "policy.1.0.coapp.installer.manifest.cdf" 
 
 echo "=== Sign Catalogs ==="
-coapp-simplesigner.exe --sign-only  "coapp.installer.cat" 
-coapp-simplesigner.exe --sign-only  "policy.1.0.coapp.installer.cat" 
+..\..\..\..\solution\tools\simplesigner\simplesigner.exe --sign-only  "coapp.installer.cat" 
+..\..\..\..\solution\tools\simplesigner\simplesigner.exe --sign-only  "policy.1.0.coapp.installer.cat" 
 
 echo "=== Create MSI ==="
 "c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" "coapp.installer.wxs "
 "c:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" "coapp.installer.wixobj"
 
 echo "=== Sign MSI ==="
-coapp-simplesigner.exe --sign-only "coapp.installer.msi"
+..\..\..\..\solution\tools\simplesigner\simplesigner.exe --sign-only "coapp.installer.msi"
 
-copy "coapp.installer.msi" ..\..\..\..\release\final
+if exist ..\..\..\..\release\final copy "coapp.installer.msi" ..\..\..\..\release\final
